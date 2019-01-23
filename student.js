8d2924fe8fd79508bf9e669578e4839a39c69161
Ileriayo Adebiyi <ileriayoadebiyi@gmail.com>
2019-01-23 06:03:27 +0100

    Create student constructor function

---
+++ student.js
@@ -0,0 +1,9 @@
+// Constructor for Student
+function Student(first_name, last_name, age, grade){
+	this.first_name = first_name;
+	this.last_name = last_name;
+	this.age = age;
+	this.grade = grade;
+}
+
+module.exports = Student;
